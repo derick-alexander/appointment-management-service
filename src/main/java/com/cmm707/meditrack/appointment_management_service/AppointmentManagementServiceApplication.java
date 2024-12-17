2a36e5dbd8b51699966f08c0df3d2551d2cb5936
derick-alexander <derickawsalexander@gmail.com>
2024-12-18 01:14:33 +0530

    first commit

---
+++ src/main/java/com/cmm707/meditrack/appointment_management_service/AppointmentManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cmm707.meditrack.appointment_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmm707/meditrack/appointment_management_service/AppointmentManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmm707.meditrack.appointment_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
